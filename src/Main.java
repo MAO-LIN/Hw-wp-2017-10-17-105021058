8f1b1aa8b5bcc4ca7ae58370eadfa14ce893bcad
SamaLin <105021058@live.asia.edu.tw>
2017-10-23 23:29:18 +0800

    finish

---
+++ src/Main.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+        LoginFrame mfr=new LoginFrame();
+        mfr.setVisible(true);
+//        Scanner scn=new Scanner(System.in);
+//        String a="abcd";
+//        String b = a.substring(a.length(),a.length());
+//        System.out.println(b);
+    }
+}
